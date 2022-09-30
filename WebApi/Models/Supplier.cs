3db950875232fba8e390141d8d5bbd70e82cc23c
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2022-09-30 19:28:34 +0300

    Add basic models.

---
+++ WebApi/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public sealed class Supplier
+    {
+        [BindNever]
+        public long SupplierId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; } = string.Empty;
+        [BindNever]
+        public IEnumerable<Product>? Products { get; set; }
+    }
+}
