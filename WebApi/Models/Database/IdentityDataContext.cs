20392a3ba47720549aa81677556502c000b45658
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2022-09-30 20:26:28 +0300

    Add database

---
+++ WebApi/Models/Database/IdentityDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models.Database
+{
+    public sealed class IdentityDataContext : IdentityDbContext
+    {
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> opts): base(opts) { }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,10 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Services
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApiDataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:Store"]!);
+});
+builder.Services.AddDbContext<IdentityDataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:Identity"]!);
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityDataContext>();
+
+#endregion
+
 var app = builder.Build();
+
+#region App
 
 if (app.Environment.IsDevelopment())
 {
@@ -17,5 +37,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
+
+ApiSeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 
 app.Run();
