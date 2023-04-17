30a734ed2f8d0cb8caa39ddd02cfb3856fd4cfae
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-04-17 18:24:29 +0300

    Replce AutoMapper with Mapster

---
+++ Infrastructure/Mapster/MapsterRegister.cs
@@ -0,0 +1,35 @@
+﻿using Core.Contracts.Controllers.Auth;
+using Core.Contracts.Controllers.Categories;
+using Core.Contracts.Controllers.Products;
+using Core.Contracts.Controllers.Suppliers;
+using Core.Entities;
+using Mapster;
+
+namespace Infrastructure.Mapster
+{
+    public sealed class MapsterRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.ForType<CreateProductRequest, Product>()
+                .Map(d => d.Images, s => s.Name + "/" + s.Img.FileName);
+            config.ForType<UpdateProductRequest, Product>()
+                .Map(d => d.Images, s => s.Img == null ? "" : s.Name + "/" + s.Img.FileName);
+
+
+            config.ForType<CreateCategoryRequest, Category>();
+            config.ForType<UpdateCategoryRequest, Category>();
+
+
+            config.ForType<CreateSupplierRequest, Supplier>();
+            config.ForType<UpdateSupplierRequest, Supplier>();
+
+
+            config.ForType<RegisterRequest, User>()
+                .Map(d => d.UserName, s => s.Username);
+
+            config.ForType<RefreshTokenRequest, RefreshToken>();
+
+        }
+    }
+}
