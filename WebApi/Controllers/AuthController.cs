1e92601adf07a933036cb83cc8dca96557c02f37
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-04-17 18:28:41 +0300

    Move AuthController logic to AuthService

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -1,15 +1,7 @@
-﻿using AutoMapper;
-using Core.Dto;
-using Core.Entities;
-using Microsoft.AspNetCore.Identity;
+﻿using Core.Contracts.Controllers.Auth;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Security.Principal;
-using System.Text;
 
 namespace WebApi.Controllers
 {
@@ -17,173 +9,90 @@ namespace WebApi.Controllers
     [ApiController]
     public sealed class AuthController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
-        private readonly UserManager<User> _userManager;
-        private readonly IMapper _mapper;
+        private readonly IAuthService _authService;
 
-        public AuthController(IConfiguration configuration, UserManager<User> userManager, IMapper mapper)
+        public AuthController(IAuthService authService)
         {
-            _configuration = configuration;
-            _userManager = userManager;
-            _mapper = mapper;
+            _authService = authService;
         }
 
         [HttpPost("login")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Login(UserDto request)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JwtResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestResult))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Login(LoginRequest request)
         {
-            var user = await _userManager.FindByNameAsync(request.Username!);
-
-            if (user == null)
+            try
             {
-                return NotFound($"No user with username {request.Username}");
+                var result = await _authService.Login(request);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+
+                return BadRequest("Invalid credentials");
             }
-            if (!await _userManager.CheckPasswordAsync(user, request.Password!))
+            catch (Exception ex)
             {
-                return BadRequest($"Invalid password");
+                return Problem(ex.Message);
             }
-
-            string token = await CreateToken(user);
-
-            var refreshToken = GenerateRefreshToken();
-
-            await UpdateUserRefreshTokenAsync(user, refreshToken);
-
-            return Ok(new JwtDto { Jwt = token, RefreshToken = refreshToken });
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(RegisterDto request)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OkResult))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestResult))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Register(RegisterRequest request)
         {
-            //var user = _mapper.Map<User>(request);
-
-            User user = new()
+            try
             {
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                UserName = request.Username,
-                Email = request.Email,
-            };
+                var result = await _authService.Register(request);
 
-            var result = await _userManager.CreateAsync(user, request.Password);
+                if (result)
+                {
+                    return Ok();
+                }
 
-            if (result.Succeeded)
-            {
+                return BadRequest();
             }
-            await _userManager.AddToRoleAsync(user, "Customer");
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
 
-
-            var temp = await _userManager.FindByNameAsync("string");
-            var roles = await _userManager.GetRolesAsync(temp!);
-
-            return Ok();
         }
 
         [HttpPost("refresh")]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> RefreshJwt([FromBody] RefreshToken refreshToken)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JwtResponse))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(UnauthorizedResult))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> RefreshJwt([FromBody] RefreshTokenRequest refreshToken)
         {
-            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken.Token && u.RefreshTokenExpired == refreshToken.Expired);
-
-            if (user != null)
-            {
-                string token = await CreateToken(user);
-
-                return Ok(new JwtDto { Jwt = token });
-            }
-
-            return Unauthorized();
-        }
-
-        [HttpPost("validate")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult Validate(JwtDto token)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var validationParameters = GetValidationParameters();
-
-            SecurityToken validatedToken;
-
             try
             {
-                IPrincipal principal = tokenHandler.ValidateToken(token.Jwt, validationParameters, out validatedToken);
+                var result = await _authService.Refresh(refreshToken);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+
+                return Unauthorized();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return BadRequest();
+                return Problem(ex.Message);
             }
-            return Ok(new JwtDto { Jwt = token.Jwt });
         }
 
-
-
-        private TokenValidationParameters GetValidationParameters()
+        [Authorize]
+        [HttpGet("protected")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OkResult))]
+        public IActionResult Protected()
         {
-            return new TokenValidationParameters()
-            {
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero,
-                ValidateAudience = false,
-                ValidateIssuer = false,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("JwtSettings:SecurityKey").Value!))
-            };
+            return Ok();
         }
 
-        private async Task<string> CreateToken(User user)
-        {
-            var roles = await _userManager.GetRolesAsync(user);
-            List<Claim> claims = new()
-            {
-                new(ClaimTypes.Name, user.UserName!),
-                new(ClaimTypes.Expiration, DateTime.Now.ToString()),
-
-            };
-            List<Claim> rolesList = new();
-
-            foreach (var role in roles)
-            {
-                rolesList.Add(new(ClaimTypes.Role, role));
-            }
-
-            claims.AddRange(rolesList);
-
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("JwtSettings:SecurityKey").Value!));
-
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(double.Parse(_configuration.GetSection("JwtSettings:ExpiresInMinutes").Value!)),
-                signingCredentials: credentials
-                );
-
-            string jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return jwt;
-        }
-
-
-        private static RefreshToken GenerateRefreshToken()
-        {
-            RefreshToken refreshToken = new()
-            {
-                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
-                Expired = DateTime.Now.AddHours(5),
-            };
-
-            return refreshToken;
-        }
-
-        private async Task UpdateUserRefreshTokenAsync(User user, RefreshToken refreshToken)
-        {
-            user.RefreshToken = refreshToken.Token;
-            user.RefreshTokenExpired = refreshToken.Expired;
-
-            await _userManager.UpdateAsync(user);
-        }
     }
 }
