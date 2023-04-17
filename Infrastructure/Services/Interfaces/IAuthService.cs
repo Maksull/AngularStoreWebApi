f7869dab5f9e152b103933e59cfef321f32922b9
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-04-17 18:26:29 +0300

    Create AuthService for AuthController

---
+++ Infrastructure/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Contracts.Controllers.Auth;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<JwtResponse?> Login(LoginRequest login);
+        Task<bool> Register(RegisterRequest register);
+        Task<JwtResponse?> Refresh(RefreshTokenRequest request);
+    }
+}
