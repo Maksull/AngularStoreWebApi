dc82aec90fdb1daafc277b8e40bdb11a972e8d94
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-04-19 09:28:13 +0300

    Update FluentValidator - Add validations for new Order DTOs

---
+++ Core/Validators/Orders/UpdateOrderRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Contracts.Controllers.Orders;
+using FluentValidation;
+
+namespace Core.Validators.Orders
+{
+    public sealed class UpdateOrderRequestValidator : AbstractValidator<UpdateOrderRequest>
+    {
+        public UpdateOrderRequestValidator()
+        {
+            RuleFor(o => o.OrderId).GreaterThanOrEqualTo(1);
+            RuleFor(o => o.Name).NotEmpty();
+            RuleFor(o => o.Email).NotEmpty();
+            RuleFor(o => o.Address).NotEmpty();
+            RuleFor(o => o.City).NotEmpty();
+            RuleFor(o => o.Country).NotEmpty();
+            RuleFor(o => o.Zip).NotEmpty();
+            RuleFor(o => o.IsShipped).NotEmpty();
+            RuleFor(o => o.Lines).NotEmpty();
+        }
+    }
+}
