3b4b95aca5a75a3f201866b155f6ac8337311944
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-05-21 16:51:02 +0300

    Update register requst. - Add Phone number field - Add validator for phone number

--- Core/Validators/FluentValidationExtensions.cs
+++ Core/Validators/FluentValidationExtensions.cs
@@ -10,5 +10,10 @@ namespace Core.Validators
             return ruleBuilder.Must(password => Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{6,}$"))
                               .WithMessage("Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
         }
+        public static IRuleBuilderOptions<T, string> MatchesUkrainianPhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(phoneNumber => Regex.IsMatch(phoneNumber, @"^\+380\d{9}$"))
+                              .WithMessage("Please enter a valid Ukrainian phone number starting with '+380'.");
+        }
     }
 }
